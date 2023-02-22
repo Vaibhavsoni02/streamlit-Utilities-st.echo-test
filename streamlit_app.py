1e56dd32ffceea933c0083cd3cc2ccb609d6509d
Vaibhav Soni <vaibhavgear7@gmail.com>
2023-02-22 19:27:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,31 @@
 # st.echo(code_location="above" or "below")
 # Whether to show the echoed code before or after the results of the executed code block.
 
-import streamlit as st
+ #import streamlit as st
 
-with st.echo():
-    st.write('This code will be printed')
+#with st.echo():
+   # st.write('This code will be printed')
+    
+    
+    import streamlit as st
+
+def get_user_name():
+    return 'John'
+
+# ------------------------------------------------
+# Want people to see this part of the code...
+
+def get_punctuation():
+    return '!!!'
+
+greeting = "Hi there, "
+user_name = get_user_name()
+punctuation = get_punctuation()
+
+st.write(greeting, user_name, punctuation)
+
+# ...up to here
+# ------------------------------------------------
+
+foo = 'bar'
+st.write('Done!')
